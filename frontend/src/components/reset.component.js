dd173cfacc619d30219daadd54414ce200550514
Kingsley <kingsleyumujeyan@gmail.com>
2020-05-18 00:42:44 +0100

    Feat_Reset password updated

--- frontend/src/components/reset.component.js
+++ frontend/src/components/reset.component.js
@@ -2,16 +2,40 @@ import React, { Component } from 'react';
 
 
 export default class Reset extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      reset_Email: ""
+    }
+    this.onChangeResetEmail = this.onChangeResetEmail.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onChangeResetEmail(e){
+    this.setState({
+      reset_Email: e.target.value
+    })
+  }
+  onSubmit(e){
+    e.preventDefault();
+    console.log(`Email Reset Form Submitted`);
+    console.log(`Reset Email: ${this.state.reset_Email}`);
+
+    /**Api call should go here using axios */
+
+    this.setState({
+      reset_Email:""
+    })
+  }
     render() {
         return (
             <div className="col-sm-12 mb-3" style={{marginTop: 10}}>
             <h3 className="text-center mb-4">Reset Password</h3>
-            <form className="mt-2 form p-4">
+            <form className="mt-2 form p-4" onSubmit={this.onSubmit}>
               <p className="text-justify">Enter your email address below and we'll send you a link to reset your password</p>  
               <div className="form-group">
                   <label>Email address</label>
                   <input className="form-control" 
-                  type="text" placeholder="joe@example.com"/>
+                  type="text" value={this.state.reset_Email} onChange={this.onChangeResetEmail} placeholder="joe@example.com"/>
               </div>
               <div className="form-group mt-4 text-center">
                 <input type="submit"value="Reset Password" className="btn btn-primary  px-5"/>
