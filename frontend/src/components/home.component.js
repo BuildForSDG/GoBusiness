18238ba427cde35a11709bae81a39b8a92f04bf8
Kingsley <kingsleyumujeyan@gmail.com>
2020-05-21 20:28:46 +0100

    bg--Header toggler updated

--- frontend/src/components/home.component.js
+++ frontend/src/components/home.component.js
@@ -8,12 +8,16 @@ import React, { Component } from 'react';
 export default class Home extends Component {
   render() {
     return (
-           
+            <div className="container">
+                <div className="row">
                 <div className="col-sm-12 text-center py-5">
                     <h2>Welcome to GoBusiness</h2>
                       <a href="/signup"><button className="btn btn-primary px-5 py-2 my-3">Get Started</button></a>
                 </div>
           
+                </div>
+            </div>
+               
            
 
     );
