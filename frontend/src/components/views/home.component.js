752fc3e90c2674656703eb6383ca850c1e1704e7
Kingsley <kingsleyumujeyan@gmail.com>
2020-05-31 00:29:41 +0100

    Updated Landing Page

--- frontend/src/components/views/home.component.js
+++ frontend/src/components/views/home.component.js
@@ -4,6 +4,14 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import UserService from '../services/auth.service';
+import Investor from '../images/investor.svg';
+import Profile from '../images/personal_data__monochromatic.svg';
+import Funding from '../images/revenue_.svg';
+import UpdateProfile from '../images/update_profile.svg';
+import InvestorSignUp from '../images/investor_data_.svg';
+import ViewSME from '../images/viewSme.svg';
+import InvestSME from '../images/handshake.svg';
+
 
 
 
@@ -13,74 +21,77 @@ export default class Home extends Component {
     return (
             <div className="container">
                 <div className="row">
-                  <div className="col-sm-12 text-center py-5">
+                  <div className="col-sm-12 text-center  hero">
+                 
                       <h1>Welcome to GoBusiness</h1>
                       <h4>A Platform for SMEs Investment</h4>
                       <NavLink to="/signup"><button className="btn btn-primary px-5 py-2 my-3 start shadow">Get Started</button></NavLink>
                   </div>
                 </div>
-                <div className="row my-5 text-center">
-                    <div className="col-sm-12 mb-3">
-                        <h3>For Businesses</h3>
+
+                <div className="row my-5 text-center card-group">
+                    <div className="col-sm-12 mb-3 biz">
+                        <h3><b>Businesses</b></h3><hr/>
                     </div>
-                    <div className="col-sm-4 col-md-4 text-center">
-                      <div className="card">
+                    <div className="col-sm-4 col-md-4 text-center my-2">
+                      <div className="card h-100">
+                        <img src={Profile} className="card-img-top" alt="Signup" />
                         <div className="card-body">
-                          <h5 className="card-title">Step 1</h5>
                           <p className="card-text steps">Get Started by Signing Up</p>
                         </div>
                       </div>
                     </div>
-                    <div className="col-sm-4 col-md-4 text-center">
-                      <div className="card">
-                      <div className="card-body">
-                          <h5 className="card-title">Step 2</h5>
+                    <div className="col-sm-4 col-md-4 text-center my-2">
+                      <div className="card h-100">
+                        <div className="card-body">
                           <p className="card-text steps">Update your Business Profile</p>
                         </div>
+                        <img src={UpdateProfile} className="card-img-top" alt="Signup" />
                       </div>
                     </div>
-                    <div className="col-sm-4 col-md-4 text-center">
-                      <div className="card">
+                    <div className="col-sm-4 col-md-4 text-center my-2">
+                      <div className="card h-100">
+                      <img src={Funding} className="card-img-top" alt="Signup" />
                       <div className="card-body">
-                          <h5 className="card-title">Step 3</h5>
+                          
                           <p className="card-text steps">Request for Investment Seed</p>
                         </div>
                       </div>
                     </div>
                 </div>
-                <div className="row my-5 text-center">
+
+                <div className="row my-5 text-center card-group">
                     <div className="col-sm-12 mb-3">
-                        <h3>For Investors</h3>
+                        <h3><b>Investors</b></h3><hr/>
                     </div>
-                    <div className="col-sm-4 col-md-4 text-center">
-                      <div className="card">
+                    <div className="col-sm-4 col-md-4 text-center my-2">
+                      <div className="card h-100">
+                     
                         <div className="card-body">
-                          <h5 className="card-title">Step 1</h5>
                           <p className="card-text steps">Get Started by Signing Up</p>
                         </div>
+                        <img src={InvestorSignUp} className="card-img-top" alt="Signup" />
                       </div>
                     </div>
-                    <div className="col-sm-4 col-md-4 text-center">
-                      <div className="card">
-                      <div className="card-body">
-                          <h5 className="card-title">Step 2</h5>
-                          <p className="card-text steps">View Different SMEs Profile</p>
+                    <div className="col-sm-4 col-md-4 text-center my-2">
+                      <div className="card h-100">
+                        <img src={ViewSME} className="card-img-top" alt="Signup" />
+                        <div className="card-body">
+                            <p className="card-text steps">View Different SMEs Profile</p>
                         </div>
                       </div>
                     </div>
-                    <div className="col-sm-4 col-md-4 text-center">
-                      <div className="card">
-                      <div className="card-body">
-                          <h5 className="card-title">Step 3</h5>
-                          <p className="card-text steps">Invest in Disered SMEs</p>
+                    <div className="col-sm-4 col-md-4 text-center my-2">
+                      <div className="card h-100">
+                        
+                        <div className="card-body">
+                            <p className="card-text steps">Invest in Selected SME</p>
                         </div>
+                        <img src={InvestSME} className="card-img-top" alt="Invest in SME" />
                       </div>
                     </div>
                 </div>
-            </div>
-               
-           
-
-    );
-  };
+              </div>
+          );
+        };
 };
