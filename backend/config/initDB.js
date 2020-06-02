4c281452eb74a655e624ee4a7a0892ff8c29560e
Kingsley <kingsleyumujeyan@gmail.com>
2020-06-02 20:44:26 +0100

    Eslint issues

--- backend/config/initDB.js
+++ backend/config/initDB.js
@@ -1,8 +1,12 @@
+/* eslint-disable consistent-return */
+/* eslint-disable max-len */
+/* eslint-disable no-console */
 const mongoose = require('mongoose');
 
 const initDB = async () => {
   const testURI = process.env.MONGODBURI_TEST || 'mongodb://localhost:27017/gobusiness_test';
-  const appURI = process.env.MONGODBURI || 'mongodb://localhost:27017/gobusiness';
+  // const appURI = process.env.MONGODBURI || 'mongodb://localhost:27017/gobusiness';
+  const appURI = process.env.MONGODBURI || 'mongodb+srv://gobusiness:gobusiness@cluster0-25ydr.mongodb.net/test?retryWrites=true&w=majority';
   const URI = process.env.NODE_ENV === 'test' ? testURI : appURI;
   try {
     await mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
