ddd85bc10304ed7e1e2732c746414d081a7f11c8
Kingsley <kingsleyumujeyan@gmail.com>
2020-06-10 11:29:43 +0100

    Updates

---
+++ precache-manifest.82286e50974e8c2cb4c9f49e34029201.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0bbabe88cccf41012916afaac6cbce6",
+    "url": "/GoBusiness/index.html"
+  },
+  {
+    "revision": "b6525b36fdab34ab26db",
+    "url": "/GoBusiness/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fd41e931a8435288dc87",
+    "url": "/GoBusiness/static/css/main.99d02fd6.chunk.css"
+  },
+  {
+    "revision": "b6525b36fdab34ab26db",
+    "url": "/GoBusiness/static/js/2.84969cbe.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/GoBusiness/static/js/2.84969cbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd41e931a8435288dc87",
+    "url": "/GoBusiness/static/js/main.aa19eea0.chunk.js"
+  },
+  {
+    "revision": "b3f34ea79eb942c80234",
+    "url": "/GoBusiness/static/js/runtime-main.9d3240f2.js"
+  },
+  {
+    "revision": "978f1f9324f43cbf670e455a949ae6b7",
+    "url": "/GoBusiness/static/media/archive.978f1f93.svg"
+  },
+  {
+    "revision": "fe8026bf9cf02ae16e27b5382bee4133",
+    "url": "/GoBusiness/static/media/avataaars (2).fe8026bf.svg"
+  },
+  {
+    "revision": "330f7b3c7d9f04a40dfe84f8af4e2f35",
+    "url": "/GoBusiness/static/media/briefcase (2).330f7b3c.svg"
+  },
+  {
+    "revision": "382987d0434fd183321b9a56f9481307",
+    "url": "/GoBusiness/static/media/businessman__monochromatic.382987d0.svg"
+  },
+  {
+    "revision": "1afd551b8a2c911c180cbf781edf5ee6",
+    "url": "/GoBusiness/static/media/dollar-sign.1afd551b.svg"
+  },
+  {
+    "revision": "b4cd0ddf24a4cc129400f7dc0d7a63d1",
+    "url": "/GoBusiness/static/media/folder.b4cd0ddf.svg"
+  },
+  {
+    "revision": "6eba0f63c1ec6e307474c8b9b4b5e9b4",
+    "url": "/GoBusiness/static/media/handshake.6eba0f63.svg"
+  },
+  {
+    "revision": "9afe0612100b190337619018e0e26d94",
+    "url": "/GoBusiness/static/media/investor_data_.9afe0612.svg"
+  },
+  {
+    "revision": "7014f04ba9ed3f73edbeb421557960cc",
+    "url": "/GoBusiness/static/media/menu.7014f04b.svg"
+  },
+  {
+    "revision": "5f473b7632f10fa7397a93be98f54644",
+    "url": "/GoBusiness/static/media/personal_data__monochromatic.5f473b76.svg"
+  },
+  {
+    "revision": "d82ddbf78a5e4fbefcb98f891f3dc450",
+    "url": "/GoBusiness/static/media/revenue_.d82ddbf7.svg"
+  },
+  {
+    "revision": "65df8ce859d1cbbb454ad9294d8af04c",
+    "url": "/GoBusiness/static/media/target.65df8ce8.svg"
+  },
+  {
+    "revision": "bec4279009f15dfa29e3a4dd8dea7fae",
+    "url": "/GoBusiness/static/media/update_profile.bec42790.svg"
+  },
+  {
+    "revision": "e33746d077e2661458ee86550eed7adc",
+    "url": "/GoBusiness/static/media/user.e33746d0.svg"
+  },
+  {
+    "revision": "0c69a73b7a00195e738ae5062363664d",
+    "url": "/GoBusiness/static/media/viewSme.0c69a73b.svg"
+  }
+]);
