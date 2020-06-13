4fdc22a97ac8c8f9987ea1886109e82026a1d606
Kingsley <kingsleyumujeyan@gmail.com>
2020-06-13 18:20:25 +0100

    Updates

---
+++ precache-manifest.7423b587a4edddb4db92fb3eab045123.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be993ef08ec2094fc77eb497cd0decbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73b7b32f0330df14fc5e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d94d1bd74a5582b56fd3",
+    "url": "/static/css/main.6da5c370.chunk.css"
+  },
+  {
+    "revision": "73b7b32f0330df14fc5e",
+    "url": "/static/js/2.60c90e00.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.60c90e00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d94d1bd74a5582b56fd3",
+    "url": "/static/js/main.9caf5a17.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  },
+  {
+    "revision": "978f1f9324f43cbf670e455a949ae6b7",
+    "url": "/static/media/archive.978f1f93.svg"
+  },
+  {
+    "revision": "fe8026bf9cf02ae16e27b5382bee4133",
+    "url": "/static/media/avataaars (2).fe8026bf.svg"
+  },
+  {
+    "revision": "330f7b3c7d9f04a40dfe84f8af4e2f35",
+    "url": "/static/media/briefcase (2).330f7b3c.svg"
+  },
+  {
+    "revision": "382987d0434fd183321b9a56f9481307",
+    "url": "/static/media/businessman__monochromatic.382987d0.svg"
+  },
+  {
+    "revision": "1afd551b8a2c911c180cbf781edf5ee6",
+    "url": "/static/media/dollar-sign.1afd551b.svg"
+  },
+  {
+    "revision": "b4cd0ddf24a4cc129400f7dc0d7a63d1",
+    "url": "/static/media/folder.b4cd0ddf.svg"
+  },
+  {
+    "revision": "6eba0f63c1ec6e307474c8b9b4b5e9b4",
+    "url": "/static/media/handshake.6eba0f63.svg"
+  },
+  {
+    "revision": "9afe0612100b190337619018e0e26d94",
+    "url": "/static/media/investor_data_.9afe0612.svg"
+  },
+  {
+    "revision": "7014f04ba9ed3f73edbeb421557960cc",
+    "url": "/static/media/menu.7014f04b.svg"
+  },
+  {
+    "revision": "5f473b7632f10fa7397a93be98f54644",
+    "url": "/static/media/personal_data__monochromatic.5f473b76.svg"
+  },
+  {
+    "revision": "d82ddbf78a5e4fbefcb98f891f3dc450",
+    "url": "/static/media/revenue_.d82ddbf7.svg"
+  },
+  {
+    "revision": "65df8ce859d1cbbb454ad9294d8af04c",
+    "url": "/static/media/target.65df8ce8.svg"
+  },
+  {
+    "revision": "bec4279009f15dfa29e3a4dd8dea7fae",
+    "url": "/static/media/update_profile.bec42790.svg"
+  },
+  {
+    "revision": "e33746d077e2661458ee86550eed7adc",
+    "url": "/static/media/user.e33746d0.svg"
+  },
+  {
+    "revision": "0c69a73b7a00195e738ae5062363664d",
+    "url": "/static/media/viewSme.0c69a73b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GoBusiness/precache-manifest.82286e50974e8c2cb4c9f49e34029201.js"
+  "/precache-manifest.7423b587a4edddb4db92fb3eab045123.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GoBusiness/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
